7a8fbaf540bfa0717807ef1d6efcc8d5bac5ac0e
Calvin Hu <fivepercentjuice@gmail.com>
2015-01-15 14:54:01 -1000

    fullscreen capture

--- src/org/thoughtcrime/securesms/ConversationActivity.java
+++ src/org/thoughtcrime/securesms/ConversationActivity.java
@@ -23,7 +23,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
@@ -36,9 +35,7 @@ import android.text.Editable;
 import android.text.InputType;
 import android.text.TextUtils;
 import android.text.TextWatcher;
-import android.util.DisplayMetrics;
 import android.util.Log;
-import android.util.TypedValue;
 import android.view.ContextThemeWrapper;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -48,13 +45,11 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnFocusChangeListener;
 import android.view.View.OnKeyListener;
-import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -62,7 +57,7 @@ import com.google.protobuf.ByteString;
 
 import org.thoughtcrime.securesms.components.EmojiDrawer;
 import org.thoughtcrime.securesms.components.EmojiToggle;
-import org.thoughtcrime.securesms.components.QuickCamera;
+import org.thoughtcrime.securesms.components.QuickMediaDrawer;
 import org.thoughtcrime.securesms.components.SendButton;
 import org.thoughtcrime.securesms.contacts.ContactAccessor;
 import org.thoughtcrime.securesms.contacts.ContactAccessor.ContactData;
@@ -134,7 +129,8 @@ import static org.whispersystems.textsecure.internal.push.PushMessageProtos.Push
  */
 public class ConversationActivity extends PassphraseRequiredActionBarActivity
     implements ConversationFragment.ConversationFragmentListener,
-               AttachmentManager.AttachmentListener
+               AttachmentManager.AttachmentListener,
+               QuickMediaDrawer.Callback
 {
   private static final String TAG = ConversationActivity.class.getSimpleName();
 
@@ -165,10 +161,9 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
   private EmojiDrawer                   emojiDrawer;
   private EmojiToggle                   emojiToggle;
   private FrameLayout                   mediaContainer;
-  private FrameLayout                   mediaCaptureControl;
-  private QuickCamera                   quickCamera;
   private ImageButton                   quickMediaButton;
-  private View                          bottomPanel;
+  private QuickMediaDrawer              quickMediaDrawer;
+  private View                          layoutContainer;
 
   private Recipients recipients;
   private long       threadId;
@@ -230,7 +225,6 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     initializeIme();
     initializeCharactersLeftViewEnabledCheck();
     calculateCharactersRemaining();
-    adjustQuickMediaOffset();
 
     MessageNotifier.setVisibleThread(threadId);
     markThreadAsRead();
@@ -241,7 +235,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     super.onPause();
     MessageNotifier.setVisibleThread(-1L);
     if (isFinishing()) overridePendingTransition(R.anim.fade_scale_in, R.anim.slide_to_right);
-    quickCamera.stopPreview();
+    quickMediaDrawer.stop();
   }
 
   @Override
@@ -547,13 +541,13 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     builder.setCancelable(true);
     builder.setMessage(R.string.ConversationActivity_are_you_sure_that_you_want_to_permanently_delete_this_conversation_question);
     builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
-      @Override
-      public void onClick(DialogInterface dialog, int which) {
-        if (threadId > 0) {
-          DatabaseFactory.getThreadDatabase(ConversationActivity.this).deleteConversation(threadId);
-          finish();
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            if (threadId > 0) {
+                DatabaseFactory.getThreadDatabase(ConversationActivity.this).deleteConversation(threadId);
+                finish();
+            }
         }
-      }
     });
 
     builder.setNegativeButton(R.string.no, null);
@@ -751,9 +745,9 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     emojiDrawer    = (EmojiDrawer) findViewById(R.id.emoji_drawer);
     emojiToggle    = (EmojiToggle) findViewById(R.id.emoji_toggle);
     mediaContainer = (FrameLayout) findViewById(R.id.media_container);
-    mediaCaptureControl = (FrameLayout) findViewById(R.id.media_capture_controls);
     quickMediaButton = (ImageButton) findViewById(R.id.quick_media_button);
-    bottomPanel = findViewById(R.id.bottom_panel);
+    quickMediaDrawer = (QuickMediaDrawer) findViewById(R.id.quick_media_drawer);
+    layoutContainer = findViewById(R.id.layout_container);
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
       emojiToggle.setVisibility(View.GONE);
@@ -775,25 +769,9 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     composeText.setOnFocusChangeListener(composeKeyPressedListener);
     emojiDrawer.setComposeEditText(composeText);
     emojiToggle.setOnClickListener(new EmojiToggleListener());
-    quickCamera = new QuickCamera(this);
-    mediaContainer.addView(quickCamera);
-    quickMediaButton.setOnClickListener(new QuickMediaButtonListener());
-  }
-
-  private void adjustQuickMediaOffset() {
-      DisplayMetrics displaymetrics = new DisplayMetrics();
-      getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
-      int pixelOffset = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
-              getResources().getDimensionPixelOffset(R.dimen.media_preview_height),
-              getResources().getDisplayMetrics());
-      int newOffset;
-      if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT)
-            newOffset = (displaymetrics.widthPixels - pixelOffset)/2;
-      else
-          newOffset = (displaymetrics.heightPixels - pixelOffset)/2;
-      LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) mediaCaptureControl.getLayoutParams();
-      layoutParams.setMargins(0, newOffset, 0, 0);
-      mediaCaptureControl.setLayoutParams(layoutParams);
+    quickMediaDrawer.setCameraContainer(mediaContainer);
+    quickMediaDrawer.setCallback(this);
+    quickMediaButton.setOnClickListener(new QuickMediaListener());
   }
 
   private void initializeResources() {
@@ -1158,8 +1136,26 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     }.execute(message);
   }
 
+    @Override
+    public void onImageCapture(Uri imageUri) {
+      addAttachmentImage(imageUri);
+      quickMediaDrawer.hide();
+    }
 
-  // Listeners
+    @Override
+    public void onSetFullScreen(boolean fullscreen) {
+        layoutContainer.setVisibility(fullscreen ? View.GONE : View.VISIBLE);
+        if (fullscreen) {
+            getSupportActionBar().hide();
+
+        }
+        else {
+            getSupportActionBar().show();
+        }
+    }
+
+
+    // Listeners
 
   private class AttachmentTypeListener implements DialogInterface.OnClickListener {
     @Override
@@ -1184,20 +1180,14 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     }
   }
 
-  private class QuickMediaButtonListener implements OnClickListener {
-      @Override
-      public void onClick(View v) {
-          if (mediaCaptureControl.getVisibility() == View.GONE) {
-              mediaCaptureControl.setVisibility(View.VISIBLE);
-              mediaContainer.setVisibility(View.VISIBLE);
-              adjustQuickMediaOffset();
-              quickCamera.startPreview();
-          } else {
-              mediaCaptureControl.setVisibility(View.GONE);
-              mediaContainer.setVisibility(View.INVISIBLE);
-              quickCamera.stopPreview();
-          }
-      }
+  private class QuickMediaListener implements OnClickListener {
+    @Override
+    public void onClick(View v) {
+      if (quickMediaDrawer.isShown())
+        quickMediaDrawer.hide();
+      else
+        quickMediaDrawer.show();
+    }
   }
 
   private class SendButtonListener implements OnClickListener, TextView.OnEditorActionListener {
--- src/org/thoughtcrime/securesms/components/QuickMediaDrawer.java
+++ src/org/thoughtcrime/securesms/components/QuickMediaDrawer.java
@@ -1,4 +1,168 @@
 package org.thoughtcrime.securesms.components;
 
-public class QuickMediaDrawer {
+import android.content.Context;
+import android.content.res.Configuration;
+import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import org.thoughtcrime.securesms.R;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class QuickMediaDrawer extends LinearLayout {
+    private final Context context;
+    private ImageButton cameraButton, audioButton;
+    private FrameLayout controlContainer, cameraContainer;
+    private QuickCamera quickCamera;
+    private View buttonBar, cameraControls;
+    private boolean fullscreen = false;
+    private Callback callback;
+
+    public QuickMediaDrawer(Context context) {
+        this(context, null);
+    }
+
+    public QuickMediaDrawer(final Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setOrientation(VERTICAL);
+        this.context = context;
+        buttonBar = inflate(this.context, R.layout.quick_media_bar, null);
+        controlContainer = (FrameLayout) inflate(this.context, R.layout.quick_media_control_container, null);
+        this.addView(buttonBar);
+        this.addView(controlContainer);
+        cameraButton = (ImageButton) findViewById(R.id.camera_button);
+        audioButton = (ImageButton) findViewById(R.id.audio_button);
+        cameraButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (controlContainer != null && cameraContainer != null) {
+                    cameraContainer.removeAllViews();
+                    quickCamera = new QuickCamera(context);
+                    cameraContainer.addView(quickCamera);
+                    initializeCameraControls();
+                    cameraContainer.setVisibility(View.VISIBLE);
+                    controlContainer.setVisibility(View.VISIBLE);
+                    adjustQuickMediaOffset();
+                }
+            }
+        });
+    }
+
+    public void setCameraContainer(FrameLayout cameraContainer) {
+        this.cameraContainer = cameraContainer;
+    }
+
+    public void setCallback(Callback callback) {
+        this.callback = callback;
+    }
+
+    public void start() {
+        if (controlContainer != null && cameraContainer != null) {
+            cameraContainer.removeAllViews();
+            quickCamera = new QuickCamera(context);
+            cameraContainer.addView(quickCamera);
+            initializeCameraControls();
+            cameraContainer.setVisibility(View.VISIBLE);
+            controlContainer.setVisibility(View.VISIBLE);
+            adjustQuickMediaOffset();
+        }
+        if (quickCamera != null)
+            quickCamera.startPreview();
+    }
+
+    public void stop() {
+        if (quickCamera != null)
+            quickCamera.stopPreview();
+        if (cameraContainer != null)
+            cameraContainer.setVisibility(View.INVISIBLE);
+        if (controlContainer != null)
+            controlContainer.setVisibility(View.GONE);
+    }
+
+    private void adjustQuickMediaOffset() {
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        int pixelOffset = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                getResources().getDimensionPixelOffset(R.dimen.media_preview_height),
+                getResources().getDisplayMetrics());
+        int newOffset;
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT)
+            newOffset = (displayMetrics.widthPixels - pixelOffset)/2;
+        else
+            newOffset = (displayMetrics.heightPixels - pixelOffset)/2;
+        LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) controlContainer.getLayoutParams();
+        layoutParams.setMargins(0, newOffset, 0, 0);
+        controlContainer.setLayoutParams(layoutParams);
+    }
+
+    private void setFullscreenCapture(boolean fullscreen) {
+        if (callback != null) callback.onSetFullScreen(fullscreen);
+        if (fullscreen) {
+            LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) controlContainer.getLayoutParams();
+            layoutParams.setMargins(0, 0, 0, 0);
+            controlContainer.setLayoutParams(layoutParams);
+        } else {
+            adjustQuickMediaOffset();
+        }
+    }
+
+    private void initializeCameraControls() {
+        cameraControls = inflate(context, R.layout.quick_camera_controls, null);
+        controlContainer.addView(cameraControls);
+        ImageButton captureButton = (ImageButton) cameraControls.findViewById(R.id.shutter_button);
+        captureButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePicture();
+            }
+        });
+        ImageButton fullscreenCaptureButton = (ImageButton) cameraControls.findViewById(R.id.fullscreen_button);
+        fullscreenCaptureButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFullscreenCapture(!fullscreen);
+                fullscreen = !fullscreen;
+            }
+        });
+    }
+
+    private void takePicture() {
+        setFullscreenCapture(false);
+        if (quickCamera != null) {
+            quickCamera.takePicture(callback);
+        }
+    }
+
+    public interface Callback {
+        public void onImageCapture(Uri imageUri);
+        public void onSetFullScreen(boolean fullscreen);
+    }
+
+    public void show() {
+        start();
+        setVisibility(View.VISIBLE);
+    }
+
+    public void hide() {
+        stop();
+        setVisibility(View.GONE);
+    }
+
+    public boolean isShown() {
+        return getVisibility() == View.VISIBLE;
+    }
 }
